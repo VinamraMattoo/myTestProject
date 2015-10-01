2162aada3426722163077af9004f600aaf33eb0a
Vinamra Mattoo <vinamra.mattoo@porteamedical.com>
2015-10-01 13:06:23 +0530

    added desc and status th in the  sys config

--- SMSEngineUI/WebContent/js/SystemJS/SystemTables.js
+++ SMSEngineUI/WebContent/js/SystemJS/SystemTables.js
@@ -1,8 +1,8 @@
-$(document)
+/*$(document)
 		.ready(
 				function() {
 					var xmlhttp = new XMLHttpRequest();
-					var url = "http://localhost:8082/TestSmsServerProject/myFirstServlet";
+					var url = "http://localhost:8080/jqueryDemo/myFirstServlet";
 
 					xmlhttp.onreadystatechange = function() {
 						if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
@@ -18,11 +18,51 @@ $(document)
 						var i;
 						var out = "<table class=\"table table-striped\" data-row-style=\"rowStyle\">";
 						out += "<thead><tr><td><strong>" + "Name"
-								+ "</strong></td><td><strong>" + "Value"
+								+ "</strong></td>" 
+								+"<td><strong>" +"Status"
+										+"</td><td><strong>" + "Value"
 								+ "</strong></td></tr></thead>";
 						for (i = 0; i < arr.length; i++) {
-							out += "<tr><td>" + arr[i].name + "</td><td>"
-									+ arr[i].value + "</td></tr>";
+							out += "<tr><td title=\"" +" data-type : "+  arr[i].dataType  +"  description : "+ arr[i].description + ".\">" + arr[i].name 
+							+ "</td><td>" +arr[i].active
+							+"</td><td>"+ arr[i].value + "</td></tr>";
+						}
+						out += "</table>";
+
+						$('#systemPanel').append(out);
+					}
+
+				});
+*/
+
+$(document)
+		.ready(
+				function() {
+					var xmlhttp = new XMLHttpRequest();
+					var url = "http://localhost:8080/jqueryDemo/myFirstServlet";
+
+					xmlhttp.onreadystatechange = function() {
+						if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+							myFunction1(xmlhttp.responseText);
+							myFunction2(xmlhttp.responseText);
+						}
+					}
+					xmlhttp.open("GET", url, true);
+					xmlhttp.send();
+
+					function myFunction1(response) {
+						var arr = JSON.parse(response);
+						var i;
+						var out = "<table class=\"table table-striped\" data-row-style=\"rowStyle\">";
+						out += "<thead><tr><td><strong>" + "Name"
+								+ "</strong></td>" 
+								+"<td><strong>" +"Status"
+										+"</td><td><strong>" + "Value"
+								+ "</strong></td></tr></thead>";
+						for (i = 0; i < arr.length; i++) {
+							out += "<tr><td title=\"" +" data-type : "+  arr[i].dataType  +"  description : "+ arr[i].description + ".\">" + arr[i].name 
+							+ "</td><td>" +arr[i].active
+							+"</td><td>"+ arr[i].value + "</td></tr>";
 						}
 						out += "</table>";
 
