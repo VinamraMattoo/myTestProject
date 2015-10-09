3a12f36b8c66146b1307fdd40f7c77e14d933468
Vinamra Mattoo <vinamra.mattoo@porteamedical.com>
2015-10-09 17:51:09 +0530

    ds

--- SMSEngineUI/WebContent/js/DOMmanipulation/login.js
+++ SMSEngineUI/WebContent/js/DOMmanipulation/login.js
@@ -1,5 +1,12 @@
-$(document).ready(function(){
-	$("#loginButton").click(function(){
-		window.open("MainPage.html","_self");
+$(document).ready(function() {
+	$("#username").blur(function() {
+		var name = $("#username").val();
+		$.get('loginServlet', {
+			username : name
+		}, function(responseText) {
+			$('#usernameInner').text(responseText);
+		});
 	});
+
+	/* window.open("MainPage.html","_self"); */
 });
